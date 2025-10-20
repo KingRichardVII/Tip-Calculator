a5ad82da4059de420b8ffba1ad413dd4c5cac83f
Richard Phan <richard.phan626@gmail.com>
2025-10-20 09:40:15 -0700

    update code

--- TipCalculator.py
+++ TipCalculator.py
@@ -4,4 +4,9 @@ def tip_calc (tip_perc, bill):
     return tip_perc * bill / 100
 
 tip_percent = float(input("Enter the tip percentage (%): "))
-bills_dollars = float(input("Enter the bill amount ($): "))
+bill_dollars = float(input("Enter the bill amount ($): "))
+
+tip_amount = tip_calc(tip_percent, bill_dollars)
+
+print('Your tip  should be $' + str(tip_amount))
+print('Your bills should be $' + str(tip_amount + bill_dollars))
